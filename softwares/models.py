983dce305870ff300e26fee66bd010b9a680941f
Sagar Yadav <sagaryadav17@outlook.com>
2021-09-17 18:16:57 +0530

    Software Model

--- softwares/models.py
+++ softwares/models.py
@@ -10,21 +10,17 @@ class OperatingSystem(models.Model):
         return self.name
 
 
-class InstallationMethod(models.Model):
-    operating_system = models.ForeignKey(
-        OperatingSystem, on_delete=models.CASCADE)
-    package_manager = models.CharField(max_length=50, blank=False, null=False)
+class PackageManager(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
 
     def __str__(self):
-        return "%s-%s" % (self.operating_system, self.package_manager)
+        return "%s-%s" % (self.package_manager)
 
 
 class Software(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
     website = models.URLField()
-    linux = models.BooleanField(default=False)
-    mac_OS = models.BooleanField(default=False)
-    windows = models.BooleanField(default=False)
+    os = models.ManyToManyField(OperatingSystem)
 
     def __str__(self):
         return self.name
@@ -32,10 +28,9 @@ class Software(models.Model):
 
 class InstallationInfo(models.Model):
     software = models.ForeignKey(Software, on_delete=models.CASCADE)
-    method = models.ForeignKey(InstallationMethod, on_delete=models.DO_NOTHING)
+    method = models.ForeignKey(PackageManager, on_delete=models.DO_NOTHING)
     package_name = models.CharField(max_length=255, blank=True, null=True)
     download_url = models.URLField(blank=True, null=True)
-    script = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return "%s-%s" % (self.software.name, self.method.operating_system.name)
--- softwares/urls.py
+++ softwares/urls.py
@@ -2,13 +2,21 @@ from django.urls import path
 from softwares.views import (
     SoftwareListView,
     SoftwareDetailView,
-    InstallationMethodListView,
+    PackageManagerListView,
     SoftwareInstallationInfoView,
 )
 
 urlpatterns = [
-    path('', SoftwareListView.as_view(), name='software-list'),
-    path('<int:id>/', SoftwareDetailView.as_view(), name='software-detail'),
-    path('<int:id>/install/', SoftwareInstallationInfoView.as_view(), name='software-installation-info'),
-    path('installation-methods/', InstallationMethodListView.as_view(), name='installation-method-list'),
+    path("", SoftwareListView.as_view(), name="software-list"),
+    path("<int:id>/", SoftwareDetailView.as_view(), name="software-detail"),
+    path(
+        "<int:id>/install/",
+        SoftwareInstallationInfoView.as_view(),
+        name="software-installation-info",
+    ),
+    path(
+        "package-managers/",
+        PackageManagerListView.as_view(),
+        name="package-managers-list",
+    ),
 ]
